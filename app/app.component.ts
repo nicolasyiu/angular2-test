b88742c6eda042545275b7824c32f93d3466b4fc
liuxi <xi.liu@abcomb.com>
2016-11-03 19:37:21 +0800

    修改样式

--- app/app.component.ts
+++ app/app.component.ts
@@ -22,9 +22,20 @@ const HEROES:Hero[] = [
     selector: 'my-app',
     template: `
         <h1>{{title}}</h1>
+        <div *ngIf="selectedHero">
+            <h2>{{selectedHero.name}} details!</h2>
+            <div><label>id: </label>{{selectedHero.id}}</div>
+            <div>
+                <label>name: </label>
+                <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+            </div>
+        </div>
+        
         <h2>My Heroes</h2>
         <ul class="heroes">
-            <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+            <li *ngFor="let hero of heroes" 
+            [class.selected]="hero === selectedHero"
+            (click)="onSelect(hero)">
                <span class="badge">{{hero.id}}</span> {{hero.name}}
             </li>
         </ul>`,
@@ -82,10 +93,10 @@ const HEROES:Hero[] = [
 
 export class AppComponent {
     title = 'Tour of Heroes';
-    hero:Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+    selectedHero:Hero;
     heroes = HEROES;
 
+    onSelect(hero:Hero):void {
+        this.selectedHero = hero;
+    }
 }
